ba1711aff103c0090054ba33ecf62aacffc6632e
Koichi.Shimomura <unieye51@gmail.com>
2022-05-30 09:20:24 +0900

    4-19 Next.jsにおけるcssスタイリング適用方法を学ぼう

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Layout from '../components/Layout'
-
-import Link from "next/link"
 
 export default function Home() {
   return (
