8631788c2dec38a8c39b1f1857362f75af2b4381
Koichi.Shimomura <unieye51@gmail.com>
2022-06-01 08:44:32 +0900

    4-22 トップページのレイアウトを更新しよう

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
+import Link from 'next/link';
 import Layout from '../components/Layout'
+
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
     <Layout>
-      <a href=''>Next.jsです</a>
+      <section className={utilStyles.headingMd}>
+        <p>
+          私はフルスタックエンジニアです。会社経営者ととして活動しています。好きなFWはLaravelです。
+        </p>
+      </section>
+
+      <section>
+        <h2>📝エンジニアのブログ</h2>
+        <div>
+          <article>
+            <Link href="/">
+              <img src="/images/thumbnail01.jpg" alt="" />
+            </Link>
+            <Link href="/">
+              <a>SSGとSSRの使い分けの場面はいつなのか？</a>
+            </Link>
+            <br />
+            <small>February 23, 2020</small>
+          </article>
+        </div>
+      </section>
+      
     </Layout>
   )
 }
