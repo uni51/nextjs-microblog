fd70f5261437a2a8aaa7d95ff589ccdbc9e8d7eb
Koichi.Shimomura <unieye51@gmail.com>
2022-06-01 12:14:25 +0900

    6-38 ブログ記事のレイアウトを訂正しよう

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,6 @@
 import Layout from "../../components/Layout";
 import { getAllPostIds, getPostData } from "../../lib/post";
+import utilStyles from "../../styles/utils.module.css";
 
 // SSG
 export async function getStaticPaths() {
@@ -25,11 +26,11 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
 	return (
 		<Layout>
-			{postData.title}
-			<br />
-			{postData.date}
-			<br />
-			{postData.blogContentHTML}
+			<article>
+				<h1 className={utilStyles.headingX1}>{postData.title}</h1>
+				<div className={utilStyles.lightText}>{postData.date}</div>
+				<div dangerouslySetInnerHTML={{ __html: postData.blogContentHTML }} />
+			</article>
 		</Layout>
 	);
 }
