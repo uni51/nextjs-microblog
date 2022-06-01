6bc8d8b0957c6fa52cf1e3378dd9f9aa40be82d0
Koichi.Shimomura <unieye51@gmail.com>
2022-06-01 11:37:43 +0900

    6-34 getStaticPathsを実際に使ってみよう

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,15 @@
 import Layout from "../../components/Layout";
+import { getAllPostIds } from "../../lib/post";
+
+// SSG
+export async function getStaticPaths() {
+	const paths = getAllPostIds();
+	
+	return {
+		paths,
+		fallback: false, // pathsに含まれていないものは404エラーになる
+	}
+}
 
 export default function Post() {
 	return (
